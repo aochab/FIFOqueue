c3918a16e00ff4d92402d069bca6e2df9e4cfdb8
aochab <47316062+aochab@users.noreply.github.com>
2019-04-03 15:28:04 +0200

    Update FIFO.h

--- FIFO.h
+++ FIFO.h
@@ -3,22 +3,22 @@
 
 typedef struct tagQFIFOItem
 {
-	int n_Key;
-	tagQFIFOItem* pNext;
+	int n_Key;				//value of item
+	tagQFIFOItem* pNext;			//pointer on next item
 }QFIFOItem;
 
 typedef struct
 {
-	QFIFOItem* pHead;
-	QFIFOItem* pTail;
+	QFIFOItem* pHead;			//first element
+	QFIFOItem* pTail;			//last element
 }QueueFIFO;
 
 int QFCreate( QueueFIFO* q );			//qeueue initiating
 int QFEmpty( QueueFIFO* q );			//if FIFO empty returned 1 else 0
-void QFEnqueue( QueueFIFO* q, int x );	//insert new item at the end
+void QFEnqueue( QueueFIFO* q, int x );		//insert new item at the end
 int QFDequeue( QueueFIFO* q );			//take out the first item
-void QFClear( QueueFIFO* q );			//clean the queue ustawia head i tail na NULLA
-void QFRemove( QueueFIFO* q );			//clears the queue (=QFClear)
-void QFDel( QueueFIFO* q );				//remove only first item
+void QFClear( QueueFIFO* q );			//clean the queue, set head and tail as NULL
+void QFRemove( QueueFIFO* q );			//clears the queue 
+void QFDel( QueueFIFO* q );			//remove only first item
 
 #endif
